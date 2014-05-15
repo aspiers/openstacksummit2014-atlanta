df1163e3bbd107a5fcae806d55e7adfb244deb0a
Ralf Haferkamp <rhafer@suse.de>
2014-05-15 06:14:45 +0200

    Don't include the admin node in list-MACs.sh
    It's disabled in the Vagrantfile

--- vagrant/list-MACs.sh
+++ vagrant/list-MACs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for node in admin controller1 controller2 compute1; do
+for node in controller1 controller2 compute1; do
     echo -n "$node: "
     vagrant ssh $node -c 'ip link show eth1 | awk "/ether/ {print \$2}"' -- -q
 done
